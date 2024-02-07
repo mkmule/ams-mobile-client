d096a5b439068dfc18fec6967e4c68689d0388e6
Serhiy <serhiy.mos@gmail.com>
2024-02-07 18:57:27 +0400

    Configured dev tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'object-curly-spacing': ['warn', 'always'],
+    'react/react-in-jsx-scope': ['off'],
+    'react/no-unstable-nested-components': ['off'],
+  },
 };
